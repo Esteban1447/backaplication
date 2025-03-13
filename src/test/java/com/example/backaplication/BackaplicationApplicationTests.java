d86752d23bfc2ee58503cad50115d24e8f0a7ecf
Esteban1447 <esteban.robledo.rc@gmail.com>
2025-03-13 17:56:12 -0500

    Creacion del proyecto

---
+++ src/test/java/com/example/backaplication/BackaplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackaplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
